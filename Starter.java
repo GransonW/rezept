261286291fb6f1bb7a789167b4082cfb5868bce3
GransonW <68745535+GransonW@users.noreply.github.com>
2020-07-27 12:28:21 +0200

    Add files via upload

---
+++ Starter.java
@@ -0,0 +1,12 @@
+
+public class Starter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		RezeptGUI meineRezepte = new RezeptGUI("Kochrezepte");
+		
+		
+	}
+
+}

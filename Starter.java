970007b4f1a22d7f81994416dbaf5565dbc313e8
GransonW <68745535+GransonW@users.noreply.github.com>
2020-07-31 10:36:55 +0200

    Add files via upload

--- Starter.java
+++ Starter.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
 
 public class Starter {
-
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		RezeptGUI meineRezepte = new RezeptGUI("Kochrezepte");
-		
+	
+
+	
 		
 	}
 
---
+++ Zutat.java
@@ -0,0 +1,26 @@
+
+public class Zutat {
+	
+String nameZutat;
+	
+	Zutat(){
+		
+	}
+	
+	Zutat(String name){
+		this.nameZutat = name;
+	}
+	
+	
+	public void setZname(String name) {
+		this.nameZutat = name;
+	}
+	
+	
+	public String getZname() {
+		return nameZutat;
+	}
+	
+	
+
+}

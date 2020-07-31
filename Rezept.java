26de8675531eb69ef412b98ec438511521289b20
GransonW <68745535+GransonW@users.noreply.github.com>
2020-07-31 10:36:16 +0200

    Add files via upload

--- Rezept.java
+++ Rezept.java
@@ -1,4 +1,51 @@
+import java.util.ArrayList;
 
 public class Rezept {
+	
+	
+	String name;
+	Object zutat;
+	String text;
+	
+	Rezept(){
+		
+	}
+	
+	Rezept(String name){
+		this.name = name;
+	}
+	
+	
+	public void setRname(String name) {
+		this.name = name;
+	}
+	
+	
+	public String getRname() {
+		return name;
+	}
+	
+	public void setZutat(Object zutat) {
+		this.zutat= zutat;
+	}
+	public Object getZutat() {
+		return zutat;
+	}
+	
+	public void setBeschreibung(String text) {
+		this.text = text;
+	}
+	
+	public String getBeschreibung() {
+		return text;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
